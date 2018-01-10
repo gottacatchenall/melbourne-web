0b430948490e5cc6e099e76f05ef98f14cca5c0c
Michael Catchen <michael@mbp.local>
2018-01-10 15:36:23 -0700

    ok

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
-const express = require('express');
-const path = require('path');
+import Express from 'express';
+import path from 'path';
+import { Sequelize } from 'sequelize';
 
-const app = express();
+const app = new Express();
 const PORT = process.env.PORT || 5000;
 
 // Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+app.use(Express.static(path.resolve(__dirname, '../react-ui/build')));
 
-// Answer API requests.
-app.get('/api', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send('{"message":"Hello from the custom server!"}');
-});
+// ========================
+//  API
+// ========================
+import incRouter from './routes/Inc.routes.js';
+import targetRouter from './routes/Targets.routes.js';
 
-// All remaining requests return the React app, so it can handle routing.
+app.use('/api', incRouter);
+app.use('/api', targetRouter);
+
+// ========================
+//  React App
+// ========================
 app.get('*', function(request, response) {
   response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
 });
